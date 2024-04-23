9f5ec8e0ba89550b1c480ffde9015843c7d732f1
Aftab Naik <137979160+Naik-Aftab@users.noreply.github.com>
2024-04-23 17:27:12 +0530

    Update page.js

--- src/app/services/Hair-Transplant-FUT/page.js
+++ src/app/services/Hair-Transplant-FUT/page.js
@@ -13,8 +13,8 @@ const HairTransplantFutPage = () => {
                         </div>
                         <div className="col-6 d-flex justify-content-center align-items-center">
                             <div className="about">
-                                <h2>Hair Transplant Fut</h2>
-                                <p>This hair transplantation method involves donor hair being removed from a small single strip of tissue from the back of the head. The donor site is closed in a fine line with well-places sutures to minimize the scar. Donor hair grafts are microscopically extracted from this tissue by our skilled hair specialist team. Grafts are then transplanted to recipient site areas created by our hair transplant surgeons in a fashion that yields the most natural, high density result.</p>
+                                <h2> Hair Transplant FUT</h2>
+                                <p>This hair transplantation method involves donor hair being removed from a small single strip of tissue from the back of the head. The donor site is closed in a fine line with well-placed sutures to minimize the scar. Donor hair grafts are microscopically extracted from this tissue by our skilled hair specialist team. Grafts are then transplanted to recipient site areas created by our hair transplant surgeons in a fashion that yields the most natural, high density result.</p>
                             </div>
                         </div>
                     </div>
@@ -27,26 +27,27 @@ const HairTransplantFutPage = () => {
                 <div className="container">
                     <h2>FUT procedure steps</h2>
                     <ul style={{ listStyleType: 'none' }}>
-                        <li>
-                            <strong>Step 1 - </strong>During a FUT hair transplant, a local anesthetic will be used to numb the areas where follicles are being extracted and implanted.
-                        </li>
-                        <li>
-                            <strong>Step 2 - </strong>The donor tissue is removed from the scalp in a single thin strip (Single-strip harvesting). The location of the donor area is carefully chosen to be a region of the scalp with maximum hair density and the best long-term stability.
-                        </li>
-                        <li>
-                            <strong>Step 3 - </strong>In Follicular Unit Transplantation, the individual follicular units (each containing from 1 to 4 hairs) are meticulously dissected from the donor strip in their naturally occurring groups under strict stereo-microscopic control.
-                        </li>
-                        <li>
-                            <strong>Step 4 - </strong>Recipient sites are made by the surgeon using a fine instrument. Recipient site creation is a critical aspect of hair restoration surgery that requires considerable experience and surgical skill and a keen aesthetic sense.
-                        </li>
-                        <li>
-                            <strong>Step 5 - </strong>Once all of the recipient sites are made, the technician team begins to place the follicular unit grafts into these pre-made sites. One-hair follicular units are used in the frontal hair line to give a soft, natural appearance and 3- and 4-hair grafts are used in the central forelock area to achieve maximum fullness.
-                        </li>
+                       <ol>
+    <li>
+        <strong>Step 1 - </strong> During a FUT hair transplant, a local anesthetic will be used to numb the areas where follicles are being extracted and implanted.
+    </li>
+    <li>
+        <strong>Step 2 - </strong>The donor tissue is removed from the scalp in a single thin strip (Single-strip harvesting). The location of the donor area is carefully chosen to be a region of the scalp with maximum hair density and the best long-term stability.
+    </li>
+    <li>
+        <strong>Step 3 - </strong>In Follicular Unit Transplantation, the individual follicular units (each containing from 1 to 4 hairs) are meticulously dissected from the donor strip in their naturally occurring groups under strict stereo-microscopic control.
+    </li>
+    <li>
+        <strong>Step 4 - </strong>Recipient sites are made by the surgeon using a fine instrument. Recipient site creation is a critical aspect of hair restoration surgery that requires considerable experience and surgical skill and a keen aesthetic sense.
+    </li>
+    <li>
+        <strong>Step 5 - </strong>Once all of the recipient sites are made, the technician team begins to place the follicular unit grafts into these pre-made sites. One-hair follicular units are used in the frontal hairline to give a soft, natural appearance and 3- and 4-hair grafts are used in the central forelock area to achieve maximum fullness.
+    </li>
+</ol>
+
                     </ul>
 
                     <h2>Indication for FUT?</h2>
-                    <p>Depending on the type and amount of breast tissue present, The Hair and Shape Clinic may use one of two male breast reduction techniques. If the gynecomastia is caused mostly by excess fat, The Hair and Shape Clinic will be able to treat the condition using only liposuction. Through a tiny incision, he will suction out the fat with a small tube called a cannula. This will leave a small incision that will go unnoticed to the untrained eye.</p>
-                    <p><strong>Patients that is suffering from the following:</strong></p>
                     <ul>
                         <li><strong>Androgenic Alopecia:</strong> Also known as male pattern baldness, it is the most common type of hair loss in men and women. It is characterized by a gradual thinning of hair, typically on the crown and temples.</li>
                         <li><strong>Male pattern baldness:</strong> Another term for Androgenic Alopecia, specifically referring to hair loss patterns typically observed in men, such as receding hairline and balding at the crown.</li>
@@ -65,9 +66,10 @@ const HairTransplantFutPage = () => {
                     </ul>
                     <h2>How to take care after Hair Transplant. Things to do post surgery.</h2>
                     <ul>
-                        <li>The patient goes home after the procedure with the instructions and everything necessary for the post-operative care at home: an antibiotic is taken for 5days , the receiving region is moisturized with a saline spray for 7 days (every 1 hours) and sterile pillow covers must be used.</li>
-                        <li>Most or all of the hair that has been transplanted will fall out within two weeks of the procedure. The aim of the procedure was of course not that hair but the hair root from which 3-4 months after the procedure new hair will begin to grow.</li>
-                        <li>The recovery is very short, usually, within 5-7 days all signs of FUE hair transplant will disappear. The success is not only the natural hair growth but the subsequent natural-looking results. The check-up, if necessary, is within a month.</li>
+                        <li>The patient goes home after the procedure with the instructions and everything necessary for the post-operative care at home: an antibiotic is taken for 5 days , the implanted area is moisturized with a saline spray for 7 days (every 30 minutes to 1 hour) and sterile pillow covers must be used.</li>
+                        <li>The hair that is transplanted in the graft normally falls within 3-4 weeks of the procedure.</li>
+                        <li>The new hair will then grow from the hair root and will be noticeable by approximately 5 months and carry on growing. A post operative check up is carried out after 6 months and an assessment of the final outcome of surgery is normally made after 12 months when the grafts are fully grown.</li>
+                        <li>The recovery is very short, usually, within 5-7 days all signs of hair transplant will disappear. The success is not only the natural hair growth but the subsequent natural-looking results. The check-up, if necessary, is within a month.</li>
                     </ul>
                 </div>
             </section>
