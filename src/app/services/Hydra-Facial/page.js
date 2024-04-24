2722184086513226a8f39c8643771b6a57c97f25
Aftab Naik <137979160+Naik-Aftab@users.noreply.github.com>
2024-04-24 15:19:21 +0530

    Update page.js

--- src/app/services/Hydra-Facial/page.js
+++ src/app/services/Hydra-Facial/page.js
@@ -28,13 +28,27 @@ const HydraFacialPage = () => {
                     <h2>What are the benefits of Hydrafacial?</h2>
                     <ul>
                         <li><b>Gentle on the skin:</b> The vacuum equipment used in hydrafacial is extremely gentle on the skin and gets rid of the gunk, clogged dirt, and oil inside the pores with ease. It cleans out the pores and helps with deeper penetration of the other creams and products applied on the skin.</li>
-                        {/* Add other list items */}
+                         <li><strong>Hydrates the skin</strong>: The hydrafacial leaves your skin moisturized and hydrated. It also makes it soft, plump, and extremely smooth after the facial. Several individuals who underwent the treatment also said that they witnessed delayed signs of ageing on their skin, such as fine lines and wrinkles.</li>
+                         <li><strong>Restores firmness</strong>: Another benefit of hydrafacial is that it makes your skin firm and tight. It helps tighten the pores to prevent them from accumulating all the dirt and oils that contribute to pimples and acne development. It also enriches the radiance of the skin and smoothens the texture for the better.</li>
+                         <li><strong>Suited for every skin type and tone</strong>: One of the most striking advantages of hydrafacial for the skin is that it suits every skin type and skin tone. It is also suitable for any age group. It not only targets the appearance of pimples and acne scarring in teenagers but also treats hyperpigmentation in middle-aged patients as well as the signs of ageing in older individuals.</li>
+                          <li><strong>Consistent results</strong>: Since it is not a technician dependent treatment, it ensures consistent results even when you do it yourself.</li>
+                          <li><strong>Safe and Effective</strong>: It doesn’t inflict pain, risks of scarring or any permanent damage to the skin. Since it includes procedures of microdermabrasion and chemical peeling, it is safe for every skin type and helps restore the natural glow and shine to the skin.</li>
+                          <li><strong>Customizable treatment</strong>: Depending on your skin type, type of skin texture, and the damage, the experts will customize the entire procedure only for you. From the composition of the chemical peel to the moisturizers used, everything is meant specifically for your skin.</li>
+                          <li><strong>Amazing for sensitive skin</strong>: It is common for people with sensitive skin to refrain from using any kinds of harsh chemicals or treatments. The benefits of hydrafacial for skin are effective on the sensitive skin type too. The vacuum technology cleanses the skin without imposing risks of skin damage or swelling.</li>
+
+{/* Add other list items */}
                     </ul>
 
                     <h2>What Does Hydrafacial Do for Your Skin?</h2>
                     <ul>
                         <li><b>Cleansing:</b> the first step of the procedure is cleansing. The specialist will likely use cleansing products meant for your skin type and then cleanse your entire face to provide a smooth canvas for further procedures in the treatment. The cleansing process often involves exfoliation as well, to get rid of the excess oils and dead skin cells in the pores.</li>
-                        {/* Add other list items */}
+                     <li><strong>Chemical peeling:</strong> In this step, a mixture of glycolic acid, salicylic acid, and other botanical extracts is used to make a chemical peel for the specific skin type of the patient. The peel is applied and left on the skin to penetrate into the deeper layers for better cleansing.</li>
+                     <li><strong>Extraction:</strong> This step uses the vacuum extraction tool to remove the dirt from inside the pores and the dead skin cells lying on the superficial layer of the skin.</li>
+                      <li><strong>Hydrating Serum:</strong> The entire procedure is then finished with the application of a hydrating serum that seeps into the layers of the skin to hydrate it from within. The serum also helps restore the supple texture of the skin and clears out any dryness or roughness.</li>
+
+    
+
+{/* Add other list items */}
                     </ul>
                 </div>
             </section>
