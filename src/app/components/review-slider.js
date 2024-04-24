235eb9e093b2dd626c504ed1f6780c7c2b368871
Aftab Naik <137979160+Naik-Aftab@users.noreply.github.com>
2024-04-24 13:42:51 +0530

    Update review-slider.js

--- src/app/components/review-slider.js
+++ src/app/components/review-slider.js
@@ -73,19 +73,7 @@ export default function Review() {
                 </div>
                 <p>
                  If you want to do hair Transplant Avanza Clinic Should be your first choice in Mumbai.
-                 <br>
-                 <br>
-                 <br>
-                 <br>
-                 <br>
-                 <br>
-                 <br>
-                 <br>
-                 <br>
-                 <br>
-                 <br>
-                 <br>
-                    
+                                    
                 </p>
               </div>
             </div>
@@ -97,8 +85,7 @@ export default function Review() {
               <div className="shadow-effect text-center p-4">
                 <div
                   className="py-2"
-                  style={{ display: "flex", justifyContent: "center" }}
-                >
+                  style={{ display: "flex", justifyContent: "center" }}>
                   <img
                     className="m-0"
                     src="/search.png"
@@ -118,16 +105,7 @@ export default function Review() {
                 </div>
                 <p>
                   I didt a lot of search and came across Avanza clinic for my hair transplant. Dr Shweta did a good job with the treatment. I felt very comfortable with the procedure and the staff is also nice.
-                 <br>
-                 <br>
-                 <br>
-                 <br>
-                 <br>
-                 <br>
-                 <br>
-                 <br>
-                 <br>
-                  
+                
                 </p>
               </div>
             </div>
@@ -160,16 +138,7 @@ export default function Review() {
                 </div>
                 <p>
                  I am very happy with the overall results and would highly recommend this clinic to anyone who wants to go in for a hair transplant.
-                 <br>
-                 <br>
-                 <br>
-                 <br>
-                 <br>
-                 <br>
-                 <br>
-                 <br>
-                 <br>
-                 <br>
+                
                  
                 </p>
               </div>
@@ -203,17 +172,7 @@ export default function Review() {
                 </div>
                 <p>
                   Excellent staff, good doctor and you offer good service to client, may be cost bit on higher side, but they maintaining standard and services.
-                  <br>
-                  <br>
-                  <br>
-                  <br>
-                  <br>
-                  <br>
-                  <br>
-                  <br>
-                  <br>
-                  <br>
-                    
+                                     
                 </p>
               </div>
             </div>
@@ -245,11 +204,7 @@ export default function Review() {
                   <span className="star">★</span>
                 </div>
                 <p>
-                 I had done my hair transplant surgery from this clinic and result is outstanding, doctors and staff
-are well qualified and well trained they are highly supportive after transplant they gives us
-continues follow up to monitor progress of surgery now in June i have done my crown area
-transplant and it is also going well there was no any complication in my both surgery I think hair
-and shape clinic is best clinic for hair transplant with reasonable price
+               I had done my hair transplant surgery from this clinic and result is outstanding, doctors and staff are well qualified and well trained they are highly supportive after transplant they gives us continues follow up to monitor progress of surgery now in June i have done my crown area transplant and it is also going well there was no any complication in my both surgery I think hair and shape clinic is best clinic for hair transplant with reasonable price
                   
                 </p>
               </div>
@@ -282,10 +237,7 @@ and shape clinic is best clinic for hair transplant with reasonable price
                   <span className="star">★</span>
                 </div>
                 <p>
-                  I did my hair transplant at the hair and shape clinic about a 7 month ago, the team at clinic is highly professional,
-                  experienced and caring they made me feel comfortable during the entire process. Now after 7 months confidently 
-                    I can say that the result have exceeded my expectation and my hairline looks natural, the density of hair improved a lot. 
-                      Thank you Team for restoring my self confidence and helping me feel like me again.
+                  I did my hair transplant at the hair and shape clinic about a 7 month ago, the team at clinic is highly professional, experienced and caring they made me feel comfortable during the entire process. Now after 7 months confidently I can say that the result have exceeded my expectation and my hairline looks natural, the density of hair improved a lot. Thank you Team for restoring my self confidence and helping me feel like me again.
                 </p>
               </div>
             </div>
