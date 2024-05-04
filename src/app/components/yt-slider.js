adc2aa364120327543edeb04db25ef9a5822d792
Aftab Naik <aftabnaik1999@gmail.com>
2024-05-04 17:46:23 +0530

    yt-slider

--- src/app/components/yt-slider.js
+++ src/app/components/yt-slider.js
@@ -1,22 +1,24 @@
-"use client";
+"use client"
 import React, { useEffect } from "react";
 import Swiper from "swiper";
-import { Autoplay } from "swiper/modules";
+import { Autoplay,Pagination } from "swiper/modules";
+// import GLightbox from 'glightbox';
 
+// import 'glightbox/dist/css/glightbox.min.css';
+import "swiper/css/pagination";
 import "swiper/css";
 
-export default function Awards() {
+export default function YtSlider() {
   useEffect(() => {
-    const swiper = new Swiper(".result-swiper", {
-      modules: [Autoplay],
+    const swiper = new Swiper(".yt-swiper", {
+      modules: [Pagination],
       loop: true,
-      autoplay: {
-        delay: 3000, // Auto slide delay in milliseconds
-        disableOnInteraction: false, // Enable autoplay even when user interacts with the slider
-      },
-      spaceBetween: 30, // Space between slides in pixels
+      // autoplay: {
+      //   delay: 3000,
+      //   disableOnInteraction: false,
+      // },
+      spaceBetween: 30,
       breakpoints: {
-        // Define settings for different screen sizes
         320: {
           slidesPerView: 1,
         },
@@ -24,34 +26,88 @@ export default function Awards() {
           slidesPerView: 2,
         },
         1024: {
-          slidesPerView: 4,
+          slidesPerView: 2,
         },
       },
+      pagination: {
+        el: ".review-pagination",
+        clickable: true,
+      },
+      allowTouchMove: true,
     });
+
+    // const lightbox = GLightbox({
+    //   selector: '.glightbox2',
+    // });
+    // return () => {
+    //   lightbox.destroy();
+    // };
+
   }, []);
+
+
 
   return (
     <section className="py-5">
       <div
-        className="container result-swiper"
+        className="container yt-swiper"
         style={{ overflow: "hidden" }}
       >
-        <h1 className="text-center pb-4 px-3" data-aos="zoom-in"><b>Awards and Recognition</b></h1>
+        <h1 className="text-center pb-4 px-3" data-aos="zoom-in"><b>Video Testimonials</b></h1>
         <div className="swiper-wrapper">
           <div className="swiper-slide d-flex justify-content-center align-items-center">
-            <img src="/award/award1.png" class="img-fluid rounded" alt="result" style={{height:'150px',width:'150px'}} />
+            {/* <a href="https://www.youtube.com/embed/YoDmEDyiz5M?si=Dchy2K5wHpDWLIg9" className="glightbox2">
+              <img src="Assets\icons\gyne.jpg" alt="image" className="img-fluid shadow rounded" />
+            </a>
+            <div style={{position:'absolute'}}>
+              <img src="Assets\icons\youtube-color-icon.webp" className="img-fluid" width={100}/>
+            </div> */}
+            <iframe className="rounded shadow" width="450" height="300" src="https://www.youtube.com/embed/YoDmEDyiz5M?si=bT1OMhQuV5z-2lb6" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
           </div>
-          <div className="swiper-slide d-flex justify-content-center align-items-center">
-            <img src="/award/award2.png" class="img-fluid rounded" alt="result" style={{height:'150px',width:'150px'}}/>
+
+          <div className="swiper-slide d-flex justify-content-center align-items-center" style={{position:'relative'}}>
+            {/* <a href="https://www.youtube.com/embed/sNLtkwPWmIw?si=uHFFVEy_eYU53KUh" className="glightbox2">
+              <img src="\Assets\icons\lipo-1.jpg" alt="image" className="img-fluid shadow rounded" />
+            </a>
+            <div style={{position:'absolute'}}>
+              <img src="Assets\icons\youtube-color-icon.webp" className="img-fluid" width={100}/>
+            </div> */}
+            <iframe className="rounded shadow" width="450" height="300" src="https://www.youtube.com/embed/sNLtkwPWmIw?si=vUQTC_fHWpOUaqdF" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
           </div>
-          <div className="swiper-slide d-flex justify-content-center align-items-center">
-            <img src="/award/award3.png" class="img-fluid rounded" alt="result" style={{height:'150px',width:'150px'}}/>
+
+          <div className="swiper-slide d-flex justify-content-center align-items-center" style={{position:'relative'}}>
+            {/* <a href="https://www.youtube.com/embed/9NAdDdYGk2E?si=Tg8sPrTzY1VIzR48" className="glightbox2">
+              <img src="Assets\icons\lipo-2.jpg" alt="image" className="img-fluid shadow rounded" />
+            </a>
+            <div style={{position:'absolute'}}>
+              <img src="Assets\icons\youtube-color-icon.webp" className="img-fluid" width={100}/>
+            </div> */}
+            <iframe className="rounded shadow" width="450" height="300" src="https://www.youtube.com/embed/9NAdDdYGk2E?si=9Qkny49WBRtdGVpf" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
           </div>
-          <div className="swiper-slide d-flex justify-content-center align-items-center">
-            <img src="/award/award4.png" class="img-fluid rounded" alt="result" style={{height:'150px',width:'150px'}}/>
+
+          <div className="swiper-slide d-flex justify-content-center align-items-center" style={{position:'relative'}}>
+            {/* <a href="https://www.youtube.com/embed/nWMU9VR4a-U?si=Aab1fE5zz2M0CThv" className="glightbox2">
+              <img src="Assets\icons\male.jpg" alt="image" className="img-fluid shadow rounded" />
+            </a>
+            <div style={{position:'absolute'}}>
+              <img src="Assets\icons\youtube-color-icon.webp" className="img-fluid" width={100}/>
+            </div> */}
+            <iframe width="450" height="300" src="https://www.youtube.com/embed/nWMU9VR4a-U?si=lprLrjv-jsKr7Vn7" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
           </div>
-          
+
+          <div className="swiper-slide d-flex justify-content-center align-items-center" style={{position:'relative'}}>
+            {/* <a href="https://www.youtube.com/embed/pXe-bBTu9Ig?si=_EEzACaEfW2S9I83" className="glightbox2">
+              <img src="Assets\icons\rhino.jpg" alt="image" className="img-fluid shadow rounded" />
+            </a>
+            <div style={{position:'absolute'}}>
+              <img src="Assets\icons\youtube-color-icon.webp" className="img-fluid" width={100}/>
+            </div> */}
+            <iframe className="rounded shadow" width="450" height="300" src="https://www.youtube.com/embed/pXe-bBTu9Ig?si=iBKxqxttLwJSRlhZ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          </div>
+
         </div>
+        <div className="review-pagination text-center pt-4"></div>
+
       </div>
     </section>
   );
